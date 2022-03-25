357c1f157a019ae8ede1120001a067174aa4c87e
Ivan <clicker.heroes.acg@gmail.com>
2022-03-25 05:10:06 +0300

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+const path = require('path')
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+            // enablePremoteMode: true,
+        }
+    })
+
+    win.loadFile('index.html')
+    win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    } else {
+        app.quit()
+    }
+})
